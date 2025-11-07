aea085895168256ced855cd60a898a6f538f6ebd
Varun <varunbansal1409@gmail.com>
2025-11-07 11:34:12 +0530

    Add files via upload

---
+++ Day 3/operator.js
@@ -0,0 +1,88 @@
+// Arithmetic Operators
+
+console.log(10 + 10);
+console.log(10 - 5);
+console.log(10 * 4);
+console.log(10 / 2);
+console.log(10 % 3);
+
+let a = 10;
+console.log(a);
+console.log(a++);
+console.log(a++);
+
+let a1 = 10;
+console.log(a1);
+let b1 = ++a1;
+console.log(b1);
+console.log(a1);
+
+let a2 = 10;
+console.log(a2);
+console.log(a2++);
+console.log(a2++);
+
+let a3 = 10;
+console.log(a3);
+let b3 = ++a3;
+console.log(b3);
+console.log(a3);
+ 
+
+// Relational Operators
+
+console.log(5 < 10);    
+console.log(10 > 12);
+console.log(12 <= 10);
+console.log(12 >= 10);
+console.log(10 == 10);
+console.log(10 === 10);
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(10 != 10);
+
+
+// Assignment Operators
+
+let m = 10;
+let n = 10;
+
+            // let m =m +  n;
+            // console.log(m); // 20 as 10+10
+
+console.log(m+=n);
+console.log(m-=n);
+console.log(m*=n);
+console.log(m/=n);
+console.log(m%=n);
+console.log(m+=n);
+
+
+// Logical Operator
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log(!true);
+console.log(!false);
+
+
+// Ternary Operator
+
+let marks = 81;
+(marks >= 90) ? console.log("Outstanding") : console.log("Good");
+
+let online = window.navigator.onLine
+
+let date = new Date()
+console.log(date);
+
+(online === true) ? console.log("online...") : console.log(date);
+
