ad09771e60e0f2bc7e6a0e10f1a7ad8c982c19d2
Varun <varunbansal1409@gmail.com>
2025-11-19 18:32:46 +0530

    Add files via upload

---
+++ Day 17/app.js
@@ -0,0 +1,21 @@
+let a = 200;
+console.log(a);
+
+//default export
+export default a;
+
+//named export
+export let b = 100;
+console.log(b);
+
+export function demo1()
+{
+    console.log("hello world");
+    
+}
+demo1()
+
+// export{
+//     b,
+//     demo1
+// }
---
+++ Day 17/app1.js
@@ -0,0 +1,11 @@
+//default import
+
+import demo from "./app.js"
+console.log(demo);
+
+//named import
+import {b , demo1} from "./app.js"
+console.log(b);
+demo1()
+
+
